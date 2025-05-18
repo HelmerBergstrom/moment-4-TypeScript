c8803da4c2f4deb8d413c44e311e19c0f6668031
Helmer Bergström <hebe2400@student.miun.se>
2025-05-18 14:08:05 +0200

    Skapar filer för uppgiften med Angular. Lägger till component course-list och route till den.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CourseListComponent } from './course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'course-list', component: CourseListComponent}
+];
