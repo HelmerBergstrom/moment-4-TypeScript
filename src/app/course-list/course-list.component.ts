afecd339e0ea4aa9379d32108806a01ed8079dda
Helmer Bergström <hebe2400@student.miun.se>
2025-05-19 14:56:29 +0200

    Skapar sökfunktion som körs direkt när användaren matar in något i sökrutan.

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -2,22 +2,36 @@ import { Component } from '@angular/core';
 import { Courses } from '../models/courses';
 import { CourseListService } from '../service/course-list.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
     
   courses: Courses[] = [];
+
+  filteredCourses: Courses[] = [];
+  filterValue: string = "";
     
   constructor(private courseListService: CourseListService) {}
 
   ngOnInit() {
     this.courseListService.getCourses().subscribe((courses) => {
       this.courses = courses;
+      this.filteredCourses = courses;
     })
+  }
+
+  filter(): void {
+    const filter = this.filterValue.toLowerCase();
+    
+    this.filteredCourses = this.courses.filter(course =>
+      course.code.toLowerCase().includes(filter) ||
+      course.coursename.toLowerCase().includes(filter)
+    );
   }
 }
