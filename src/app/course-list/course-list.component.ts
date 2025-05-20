40833c1f13341d2a16d7f2105ac16414c104b483
Helmer Bergström <hebe2400@student.miun.se>
2025-05-20 13:47:57 +0200

    Skapar metod för att sortera tabellen med klick på <th>-elementen, förutom kursplan. Använder if-satser för att köra fallande och stigande sortering. Lägger till pilar i element för att visa vilken ordning som visas.

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -16,6 +16,9 @@ export class CourseListComponent {
 
   filteredCourses: Courses[] = [];
   filterValue: string = "";
+
+  sortCol: string = '';
+  sortAsc: boolean = true;
     
   constructor(private courseListService: CourseListService) {}
 
@@ -27,11 +30,50 @@ export class CourseListComponent {
   }
 
   filter(): void {
+    // Skapar variabel för att kunna köra filtreringen två gånger
     const filter = this.filterValue.toLowerCase();
-    
+
+    // Filtrerar tabellen med värdet som frasen som angetts.
     this.filteredCourses = this.courses.filter(course =>
       course.code.toLowerCase().includes(filter) ||
       course.coursename.toLowerCase().includes(filter)
     );
   }
-}
+
+  // Column = något i Courses, code, coursename eller progression.
+  sortBy(column: keyof Courses): void {
+    // Växlar mellan stigande och fallande. Stigande är True. Fallande är False. 
+    // Stigande ordning på första klicket på en ny kolumn i tabellen.
+    if(this.sortCol === column) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortCol = column;
+      this.sortAsc = true;
+    }
+
+    // aCourse och bCourse är två kurser som jämförs. Detta sker med alla kurser i tabelen.
+    // Konverterar till små bokstäver för jämförelsen.
+    this.filteredCourses.sort((a, b) => {
+      const aCourse = a[column]?.toLowerCase?.() ?? a[column];
+      const bCourse = b[column]?.toLowerCase?.() ?? b[column];
+
+      // Kontrollerar om a ska gå före b och tvärtom.
+      if(aCourse < bCourse)
+        return this.sortAsc ? -1 : 1;
+      if(aCourse > bCourse)
+        return this.sortAsc ? 1 : -1;
+        
+      return 0; // Går det inte att ordna kurser, returneras noll, alltså sker inget.
+    });
+  };
+
+  // Sorteringspilar som visar olika beroende på fallande/stigande ordning.
+  sortArrow(column: string): string {
+    if(this.sortCol !== column)
+      return ''; // Ingen pil som standard.
+
+    return this.sortAsc ? '⬇️' : '⬆️';
+    // Pil nedåt för fallande och uppåt för stigande.
+    // Växlar mellan dessa två pilar.
+  }
+};
